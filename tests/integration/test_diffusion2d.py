b0894d406d34347d446e9cb6d7698c356fb0bfe2
Felix Kimmerle <FelixKimmerle@hotmail.com>
2026-01-17 14:09:31 +0100

    finished integration tests

--- tests/integration/test_diffusion2d.py
+++ tests/integration/test_diffusion2d.py
@@ -2,18 +2,60 @@
 Tests for functionality checks in class SolveDiffusion2D
 """
 
+import pytest
+import numpy as np
 from diffusion2d import SolveDiffusion2D
 
+@pytest.fixture
+def solver():
+    solver = SolveDiffusion2D()
+    return solver
 
-def test_initialize_physical_parameters():
+def test_initialize_physical_parameters(solver):
     """
     Checks function SolveDiffusion2D.initialize_domain
     """
-    solver = SolveDiffusion2D()
+    w = 20.
+    h = 15.
+    dx = 0.2
+    dy = 0.3
+    d = 5.
+    T_cold = 200.
+    T_hot = 500.
+
+    solver.initialize_domain(w,h,dx,dy)
+    solver.initialize_physical_parameters(d,T_cold,T_hot)
+
+    expected_dt = 0.00276923076923077
+
+    assert solver.dt == pytest.approx(expected_dt), "dt should be approximately {expected_dt}"
 
 
-def test_set_initial_condition():
+def test_set_initial_condition(solver):
     """
     Checks function SolveDiffusion2D.get_initial_function
     """
-    solver = SolveDiffusion2D()
+    w = 20.
+    h = 15.
+    dx = 0.2
+    dy = 0.3
+    d = 5.
+    T_cold = 200.
+    T_hot = 500.
+
+    solver.initialize_domain(w,h,dx,dy)
+    solver.initialize_physical_parameters(d,T_cold,T_hot)
+    computed_u = solver.set_initial_condition()
+
+    expected_u = solver.T_cold * np.ones((solver.nx, solver.ny))
+
+    r, cx, cy = 2, 5, 5
+    r2 = r ** 2
+    for i in range(solver.nx):
+        for j in range(solver.ny):
+            p2 = (i * solver.dx - cx) ** 2 + (j * solver.dy - cy) ** 2
+            if p2 < r2:
+                expected_u[i, j] = solver.T_hot
+
+    assert np.array_equal(expected_u, computed_u), "Initial condition array is not equal to the expected"
+
--- tests/unit/test_diffusion2d_functions.py
+++ tests/unit/test_diffusion2d_functions.py
@@ -2,6 +2,7 @@
 Tests for functions in class SolveDiffusion2D
 """
 
+import numpy as np
 import pytest
 from diffusion2d import SolveDiffusion2D
 
@@ -45,3 +46,24 @@ def test_set_initial_condition():
     Checks function SolveDiffusion2D.get_initial_function
     """
     solver = SolveDiffusion2D()
+    solver.dx = 0.2
+    solver.dy = 0.3
+    solver.nx = 100
+    solver.ny = 50
+    solver.T_cold = 200.
+    solver.T_hot = 500.
+    solver.dt = 0.00276923076923077
+    solver.D = 5.
+    
+    expected_u = solver.T_cold * np.ones((solver.nx, solver.ny))
+
+    r, cx, cy = 2, 5, 5
+    r2 = r ** 2
+    for i in range(solver.nx):
+        for j in range(solver.ny):
+            p2 = (i * solver.dx - cx) ** 2 + (j * solver.dy - cy) ** 2
+            if p2 < r2:
+                expected_u[i, j] = solver.T_hot
+
+    computed_u = solver.set_initial_condition()
+    assert np.array_equal(expected_u, computed_u), "Initial condition array is not equal to the expected"
